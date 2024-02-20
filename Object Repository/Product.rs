9f843086f0bdf6088b281a715f03a7dae6bd6966
Cintya Nainggolan <cintyaysn277@gmail.com>
2024-02-20 20:56:42 +0700

    KatalonProject

---
+++ Object Repository/Product.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Product</name>
+   <tag></tag>
+   <elementGuidId>cf7db122-e807-48c2-af01-13fb073bce13</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[(text() = '${productName}' or . = '${productName}')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>${productName}</value>
+      <webElementGuid>ce2780b4-3f73-4e05-8041-177694d45d70</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
